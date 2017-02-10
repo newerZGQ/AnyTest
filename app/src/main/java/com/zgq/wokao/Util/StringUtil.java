25f6d6e92dc857c30dd8357985a70d274add5660
zgq <379020727@qq.com>
2017-02-11 00:10:35 +0800

    字符串工具类

---
+++ app/src/main/java/com/zgq/wokao/Util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.zgq.wokao.Util;
+
+import android.content.Context;
+
+/**
+ * Created by zgq on 2017/2/10.
+ */
+
+public class StringUtil {
+    public static String getResString(Context context,int id){
+        return context.getResources().getString(id);
+    }
+}
