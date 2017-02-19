74a0016424e7d78e2f26d2f9feb518be1b5dbbe3
张国强 <zhangguoqiang@qiyi.com>
2017-02-19 14:30:06 +0800

    discussadapter

--- app/src/main/java/com/zgq/wokao/parser/adapter/impl/DiscussAdapter.java
+++ app/src/main/java/com/zgq/wokao/parser/adapter/impl/DiscussAdapter.java
@@ -1,10 +1,15 @@
 package com.zgq.wokao.parser.adapter.impl;
 
+import com.zgq.wokao.Util.ListUtil;
 import com.zgq.wokao.model.paper.DiscussQuestion;
 import com.zgq.wokao.model.paper.Question;
 import com.zgq.wokao.model.paper.QuestionType;
+import com.zgq.wokao.model.paper.TFQuestion;
 import com.zgq.wokao.parser.adapter.BaseAdapter;
 import com.zgq.wokao.parser.adapter.IDiscussAdapter;
+import com.zgq.wokao.parser.context.QuestionContext;
+import com.zgq.wokao.parser.context.item.QuestionItem;
+import com.zgq.wokao.parser.context.item.QuestionItemType;
 
 import java.util.ArrayList;
 
@@ -14,7 +19,18 @@ import java.util.ArrayList;
 
 public class DiscussAdapter extends BaseAdapter implements IDiscussAdapter {
 
-    private QuestionType type = QuestionType.fillin;
+    private QuestionType type = QuestionType.disc;
+    ArrayList<String> content = new ArrayList<>();
+    QuestionContext context = new QuestionContext();
+    private ArrayList<DiscussQuestion> results = new ArrayList<>();
+
+    public DiscussAdapter() {
+        initParam();
+    }
+
+    private void initParam() {
+        context.init(5);
+    }
 
     @Override
     public QuestionType getType() {
@@ -23,6 +39,83 @@ public class DiscussAdapter extends BaseAdapter implements IDiscussAdapter {
 
     @Override
     public ArrayList<DiscussQuestion> parse(String resource) {
-        return null;
+        String[] strings = resource.split("\n");
+        content = (ArrayList<String>) ListUtil.array2list(strings);
+        parseRes(content);
+        return results;
+    }
+
+    private ArrayList<DiscussQuestion> parseRes(ArrayList<String> content) {
+        //题号
+        int number = 1;
+        String bodyString = "";
+        StringBuilder builder = new StringBuilder();
+//        System.out.println("---->>content"+content.size());
+        for (int i = 0; i < content.size(); i++) {
+            String tmp = content.get(i);
+            //如果这一行为空，则继续，不做任何操作
+            if (tmp.equals("")) {
+                continue;
+            }
+            //如果这一行是题目开始的地方
+            if (isQstNumber(tmp)) {
+                System.out.println("---->>is number"+tmp);
+                //把上一次循环的题干和答案提取出来
+                bodyString = builder.toString();
+                if (bodyString.equals("")){
+
+                }else{
+                    DiscussQuestion question = parseSingle(number, bodyString);
+                    if (question != null) {
+                        results.add(question);
+                        number++;
+                    }
+                }
+                if (builder.length() != 0) {
+                    builder.delete(0, builder.length());
+                }
+                String contentTmp = trimNum(tmp);
+                builder.append(contentTmp);
+            } else {
+                System.out.println("---->>not number"+tmp);
+                builder.append("\n" + tmp);
+            }
+        }
+
+        DiscussQuestion question = parseSingle(number, builder.toString());
+        if (question != null) {
+            results.add(question);
+        }
+        return results;
+    }
+
+    private DiscussQuestion parseSingle(int number, String questionRes) {
+        System.out.println("---->>single" + questionRes);
+        DiscussQuestion question = new DiscussQuestion();
+        question.setId(number);
+        inContext(QuestionItemType.number);
+        String[] resArray = trimNum(questionRes).split("\n");
+        StringBuilder builder = new StringBuilder();
+        for (String tmp : resArray) {
+            tmp = tmp.trim();
+            if (tmp.startsWith("答案")) {
+                question.setBody(builder.toString());
+                inContext(QuestionItemType.body);
+                String answerTmp = tmp.substring(2).trim();
+                if (answerTmp.startsWith(":") || answerTmp.startsWith("：")) {
+                    question.setAnswer(answerTmp.substring(1).trim());
+                } else {
+                    question.setAnswer(answerTmp);
+                }
+                inContext(QuestionItemType.answer);
+                continue;
+            }
+            builder.append(tmp);
+        }
+        return question;
+    }
+
+    private void inContext(QuestionItemType type) {
+        context.inContext(new QuestionItem(type));
     }
 }
