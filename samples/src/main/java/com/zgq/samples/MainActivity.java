6526511effae56dd5cdb9dbe946fa1cebbf42b80
zgq <379020727@qq.com>
2017-02-21 09:12:12 +0800

    sample

--- samples/src/main/java/com/zgq/samples/MainActivity.java
+++ samples/src/main/java/com/zgq/samples/MainActivity.java
@@ -2,12 +2,28 @@ package com.zgq.samples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.zgq.pulltomenu.PullToMenuView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PullToMenuView pullToMenuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pullToMenuView = (PullToMenuView) findViewById(R.id.pull);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        pullToMenuView.initPosition();
     }
 }
