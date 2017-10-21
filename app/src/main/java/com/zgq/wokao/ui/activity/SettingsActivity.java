a8c4a2a83b73c0a76891365a2ee74e033c45ca5a
张国强 <zhangguoqiang@qiyi.com>
2017-10-21 13:59:22 +0800

    settings

---
+++ app/src/main/java/com/zgq/wokao/ui/activity/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.zgq.wokao.ui.activity;
+
+import android.os.Bundle;
+
+import com.zgq.wokao.R;
+
+/**
+ * Created by zhangguoqiang on 2017/10/21.
+ */
+
+public class SettingsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
