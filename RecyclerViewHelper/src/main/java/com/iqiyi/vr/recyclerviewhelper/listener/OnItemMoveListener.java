ed2f153e71b4632b884f3f91fb00056645211788
张国强 <zhangguoqiang@qiyi.com>
2017-11-12 13:58:39 +0800

    add recycleviewhelper

---
+++ RecyclerViewHelper/src/main/java/com/iqiyi/vr/recyclerviewhelper/listener/OnItemMoveListener.java
@@ -0,0 +1,16 @@
+package com.iqiyi.vr.recyclerviewhelper.listener;
+
+/**
+ * Created by wangyancong on 2017/8/23.
+ * Item 移动监听
+ */
+
+public interface OnItemMoveListener {
+    /**
+     * Item 移动
+     *
+     * @param fromPosition 初始位置
+     * @param toPosition   移动位置
+     */
+    void onItemMove(int fromPosition, int toPosition);
+}
