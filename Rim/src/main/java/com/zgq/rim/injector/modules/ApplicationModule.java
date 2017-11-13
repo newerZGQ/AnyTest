04021b5f9a9098d94ee9743762c97feaa95642de
张国强 <zhangguoqiang@qiyi.com>
2017-11-13 09:38:26 +0800

    format code

--- Rim/src/main/java/com/zgq/rim/injector/modules/ApplicationModule.java
+++ Rim/src/main/java/com/zgq/rim/injector/modules/ApplicationModule.java
@@ -2,13 +2,9 @@ package com.zgq.rim.injector.modules;
 
 import android.app.Application;
 import android.content.Context;
-import com.zgq.rim.RimApplication;
-
-import javax.inject.Singleton;
 
 import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 @Module
 public abstract class ApplicationModule {
