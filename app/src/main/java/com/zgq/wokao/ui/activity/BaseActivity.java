b0bf3f2c60fe74d277f93c7295df161d10828d96
张国强 <zhangguoqiang@qiyi.com>
2017-11-15 20:54:18 +0800

    请求权限

--- app/src/main/java/com/zgq/wokao/ui/activity/BaseActivity.java
+++ app/src/main/java/com/zgq/wokao/ui/activity/BaseActivity.java
@@ -1,7 +1,9 @@
 package com.zgq.wokao.ui.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Parcelable;
+import android.support.v7.app.AlertDialog;
 
 import com.zgq.wokao.model.paper.QuestionType;
 
@@ -19,5 +21,29 @@ public class BaseActivity extends AbsActivity {
         if (finishThis) {
             finish();
         }
+    }
+
+    protected void showBaseDialog(String title, String message,
+                              String positive, String negative, final OnOprateDialogListener listener) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.onClickPositive();
+                    }
+                })
+                .setNegativeButton(negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.onClickNegative();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    public interface OnOprateDialogListener {
+        void onClickPositive();
+        void onClickNegative();
     }
 }
