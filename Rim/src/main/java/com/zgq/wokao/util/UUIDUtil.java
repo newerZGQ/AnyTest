53f04acdf0d3123e02c292e255703e9a12f6f6bf
张国强 <zhangguoqiang@qiyi.com>
2017-11-20 19:47:35 +0800

    utils

--- Rim/src/main/java/com/zgq/wokao/util/UUIDUtil.java
+++ Rim/src/main/java/com/zgq/wokao/util/UUIDUtil.java
@@ -2,10 +2,6 @@ package com.zgq.wokao.util;
 
 import java.util.UUID;
 
-/**
- * Created by zgq on 2017/2/11.
- */
-
 public class UUIDUtil {
     public static String getID() {
         return UUID.randomUUID().toString().replace("-", "");
