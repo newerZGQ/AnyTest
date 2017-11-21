87b690bf08ec1921b22578336b02335bf7f7a4ad
张国强 <zhangguoqiang@qiyi.com>
2017-11-21 10:32:12 +0800

    logger config

--- Rim/src/main/java/com/zgq/wokao/module/home/HomeActivity.java
+++ Rim/src/main/java/com/zgq/wokao/module/home/HomeActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +27,8 @@ import javax.inject.Inject;
 import butterknife.BindView;
 
 public class HomeActivity extends BaseActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     @BindView(R.id.root_view)
     View rootView;
