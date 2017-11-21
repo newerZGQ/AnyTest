4151ce3887e14fbc187cb52f60ce5b3a40001776
张国强 <zhangguoqiang@qiyi.com>
2017-11-21 10:04:45 +0800

    home

--- Rim/src/main/java/com/zgq/wokao/module/home/HomeActivity.java
+++ Rim/src/main/java/com/zgq/wokao/module/home/HomeActivity.java
@@ -1,13 +1,53 @@
 package com.zgq.wokao.module.home;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
+import com.zgq.linechart.ChartView;
 import com.zgq.wokao.R;
 import com.zgq.wokao.module.base.BaseActivity;
+import com.zgq.wokao.widget.CustomViewPager;
+import com.zgq.wokao.widget.SlideUp;
 
-/**
- * Created by zgq on 2017/11/21.
- */
+import butterknife.BindView;
 
 public class HomeActivity extends BaseActivity {
+
+    @BindView(R.id.root_view)
+    View rootView;
+    @BindView(R.id.main_layout)
+    RelativeLayout mainLayout;
+    @BindView(R.id.menu_layout)
+    RelativeLayout menuLayout;
+    @BindView(R.id.toolbar_menu)
+    Button menuBtn;
+    @BindView(R.id.share)
+    Button shareApp;
+    @BindView(R.id.toolbar_search)
+    Button searchBtn;
+    @BindView(R.id.toolbar_layout)
+    RelativeLayout toolbarLayout;
+    SlideUp slideUp;
+    @BindView(R.id.home_tab)
+    NavigationTabStrip tabStrip;
+    @BindView(R.id.viewpaper)
+    CustomViewPager viewPager;
+    @BindView(R.id.line_chart)
+    ChartView lineChart;
+    @BindView(R.id.toolbar_add)
+    Button parseBtn;
+    @BindView(R.id.total_count)
+    TextView totalCount;
+    @BindView(R.id.total_accuracy)
+    TextView totalAccuracy;
+
     @Override
     protected int attachLayoutRes() {
         return R.layout.activity_home;
--- Rim/src/main/java/com/zgq/wokao/module/home/HomeContract.java
+++ Rim/src/main/java/com/zgq/wokao/module/home/HomeContract.java
@@ -1,7 +1,11 @@
 package com.zgq.wokao.module.home;
 
+import com.zgq.wokao.entity.summary.StudySummary;
+import com.zgq.wokao.entity.summary.TotalDailyCount;
 import com.zgq.wokao.module.BasePresenter;
 import com.zgq.wokao.module.BaseView;
+
+import io.realm.RealmList;
 
 /**
  * Created by zhangguoqiang on 2017/11/12.
@@ -9,8 +13,11 @@ import com.zgq.wokao.module.BaseView;
 
 public interface HomeContract {
     interface View extends BaseView<Presenter>{
-
+        void showDayCurve(StudySummary studySummary);
+        void showTotalRecord(StudySummary studySummary);
     }
 
-    interface Presenter extends BasePresenter<View> {}
+    interface Presenter extends BasePresenter<View> {
+
+    }
 }
