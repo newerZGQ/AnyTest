d9c89a15e11d6d717ceab87413bbda5641f997a8
张国强 <zhangguoqiang@qiyi.com>
2017-11-23 15:02:24 +0800

    lamda

--- Rim/src/main/java/com/zgq/wokao/module/parser/ParserPresenter.java
+++ Rim/src/main/java/com/zgq/wokao/module/parser/ParserPresenter.java
@@ -1,8 +1,5 @@
 package com.zgq.wokao.module.parser;
 
-import com.google.common.base.Optional;
-import com.orhanobut.logger.Logger;
-import com.zgq.wokao.entity.paper.NormalExamPaper;
 import com.zgq.wokao.module.base.BasePresenter;
 import com.zgq.wokao.parser.ParserHelper;
 import com.zgq.wokao.repository.RimRepository;
@@ -10,10 +7,7 @@ import com.zgq.wokao.repository.RimRepository;
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class ParserPresenter extends BasePresenter<ParserContract.View>
@@ -30,25 +24,17 @@ public class ParserPresenter extends BasePresenter<ParserContract.View>
         view.showLoading();
         Observable.just(filePath)
                 .subscribeOn(Schedulers.io())
-                .flatMap(new Function<String, ObservableSource<Optional<NormalExamPaper>>>() {
-                    @Override
-                    public ObservableSource<Optional<NormalExamPaper>> apply(String s) throws Exception {
-                        return Observable.just(ParserHelper.getInstance().parse(filePath));
-                    }
-                })
+                .flatMap(s -> Observable.just(ParserHelper.getInstance().parse(filePath)))
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Optional<NormalExamPaper>>() {
-                    @Override
-                    public void accept(Optional<NormalExamPaper> normalExamPaperOptional) throws Exception {
-                        if (normalExamPaperOptional.isPresent()){
-                            view.showSuccessToast();
-                            repository.saveExamPaper(normalExamPaperOptional.get());
-                            repository.setSked(normalExamPaperOptional.get(), true);
-                        }else{
-                            view.showFailedToast();
-                        }
-                        view.destoryView();
+                .subscribe(normalExamPaperOptional -> {
+                    if (normalExamPaperOptional.isPresent()){
+                        view.showSuccessToast();
+                        repository.saveExamPaper(normalExamPaperOptional.get());
+                        repository.setSked(normalExamPaperOptional.get(), true);
+                    }else{
+                        view.showFailedToast();
                     }
+                    view.destoryView();
                 });
     }
 
