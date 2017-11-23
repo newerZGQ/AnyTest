c44bc69fab6fbe5afaf43dbd7e1ea98f3f98ab37
张国强 <zhangguoqiang@qiyi.com>
2017-11-23 15:02:04 +0800

    repo

--- Rim/src/main/java/com/zgq/wokao/repository/RimRepository.java
+++ Rim/src/main/java/com/zgq/wokao/repository/RimRepository.java
@@ -1,17 +1,24 @@
 package com.zgq.wokao.repository;
 
+import com.zgq.wokao.dao.RimDaoSource;
 import com.zgq.wokao.entity.paper.NormalExamPaper;
 
 import com.google.common.base.Optional;
 import com.zgq.wokao.entity.summary.StudySummary;
 
+import java.util.List;
+
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
 import io.reactivex.Flowable;
-import io.reactivex.functions.Consumer;
+import io.realm.RealmModel;
+import io.realm.RealmObject;
 
 public class RimRepository implements RimDataSource {
+
+    @Inject
+    RimDaoSource daoSource;
 
     private PaperRepository paperRepository;
 
@@ -42,12 +49,9 @@ public class RimRepository implements RimDataSource {
 
     @Override
     public void saveSummary(@Nonnull final StudySummary studySummary) {
-        getStudySummary().subscribe(new Consumer<Optional<StudySummary>>() {
-            @Override
-            public void accept(Optional<StudySummary> studySummaryOptional) throws Exception {
-                if (!studySummaryOptional.isPresent()){
-                    paperRepository.saveSummary(studySummary);
-                }
+        getStudySummary().subscribe(studySummaryOptional -> {
+            if (!studySummaryOptional.isPresent()){
+                paperRepository.saveSummary(studySummary);
             }
         });
     }
@@ -55,5 +59,15 @@ public class RimRepository implements RimDataSource {
     @Override
     public Flowable<Optional<StudySummary>> getStudySummary() {
         return paperRepository.getStudySummary();
+    }
+
+    @Override
+    public <T extends RealmModel> Flowable<T> copyFromRealm(T t) {
+        return Flowable.just(daoSource.copyFromRealm(t));
+    }
+
+    @Override
+    public <T extends RealmModel> Flowable<List<T>> copyFromRealm(Iterable<T> realmObjects) {
+        return Flowable.just(daoSource.copyFromRealm(realmObjects));
     }
 }
