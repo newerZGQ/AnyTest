07ef40735b14dcfbf80ec1049124ded7385fd02f
zgq <zhanggq.work@gmail.com>
2020-07-26 18:49:04 +0800

    home view

--- rim/src/main/java/com/zgq/wokao/module/maintab/homepage/HomepageFragment.java
+++ rim/src/main/java/com/zgq/wokao/module/maintab/homepage/HomepageFragment.java
@@ -1,6 +1,10 @@
 package com.zgq.wokao.module.maintab.homepage;
 
+import android.graphics.Outline;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 import android.view.ViewStub;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,6 +25,21 @@ public class HomepageFragment extends BaseFragment<MainTabContract.HomepagePrese
     @BindView(R.id.toolbar_title)
     TextView toolbarTitle;
 
+    @BindView(R.id.toolbar_switch)
+    TextView toolbarSwitch;
+
+    @BindView(R.id.study_detail)
+    ViewGroup studyDetail;
+
+    @BindView(R.id.recent_tag)
+    TextView recentTag;
+
+    @BindView(R.id.start_study)
+    TextView startStudy;
+
+    @BindView(R.id.study_record)
+    ViewGroup studyRecord;
+
     @Override
     protected void daggerInject() {
         DaggerMaintabComponent.builder()
@@ -37,6 +56,41 @@ public class HomepageFragment extends BaseFragment<MainTabContract.HomepagePrese
 
     @Override
     protected void initViews() {
-        toolbarTitle.setTextColor(getResources().getColor(R.color.colorWhite));
+        toolbarSwitch.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), view.getHeight()/2);
+            }
+        });
+        toolbarSwitch.setClipToOutline(true);
+
+        studyDetail.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), view.getHeight()/10);
+            }
+        });
+        studyDetail.setClipToOutline(true);
+        recentTag.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0,0,view.getWidth(), view.getHeight(), view.getHeight()/8);
+            }
+        });
+        recentTag.setClipToOutline(true);
+        startStudy.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0,0,view.getWidth(),view.getHeight(),view.getHeight()/5);
+            }
+        });
+        startStudy.setClipToOutline(true);
+        studyRecord.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0,0,view.getWidth(),view.getHeight(),view.getHeight()/10);
+            }
+        });
+        studyRecord.setClipToOutline(true);
     }
 }
